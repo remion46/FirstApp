373084166981ba219870c728ea5d07d39bc3253d
remion <ren.waseda@gmail.com>
2021-02-13 21:59:48 +0900

    title画面つなぐ前

---
+++ ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firstapp
+//
+//  Created by 船越廉 on 2021/02/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
