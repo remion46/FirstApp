d1f988001ae0397b265e2489b02a4148c7558f2c
remion <ren.waseda@gmail.com>
2021-08-18 13:22:22 +0900

    202108181330

--- FirstApp/Firstapp/ColumnViewController.swift
+++ FirstApp/Firstapp/ColumnViewController.swift
@@ -2,56 +2,22 @@
 //  ColumnViewController.swift
 //  Firstapp
 //
-//  Created by 船越廉 on 2021/06/15.
+//  Created by 船越廉 on 2021/08/18.
 //
 
 import UIKit
+import WebKit
 
 class ColumnViewController: UIViewController {
+    @IBOutlet weak var webview: WKWebView!
     
-    @IBOutlet weak var tableView: UITableView!
-
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        if let url = URL(string: "https://remion46.hatenablog.com/") {  // URL文字列の表記間違いなどで、URL()がnilになる場合があるため、nilにならない場合のみ以下のload()が実行されるようにしている
+            self.webview.load(URLRequest(url: url))
         // Do any additional setup after loading the view.
-        tableView.register(UINib(nibName: "columnviewcell", bundle: nil), forCellReuseIdentifier: "columnviewcell")
-    }
-}
+        }
+    
 
-extension ColumnViewController: UITableViewDataSource {
-
-    // セクションの個数を返す
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    // セクションごとにセルの個数を返す
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-
-    // セルの中身を返す
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "columnviewcell")!
-        return cell
-    }
-
-    // セルの高さを返す
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 1000
-    }
-}
-
-extension ColumnViewController: UITableViewDelegate {
-
-    // セルがタップされたときの処理を書く
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        return
-    }
-
-    // スクロールしたときの処理を書く
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        return
     }
 }
